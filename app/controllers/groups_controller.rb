1c323ac10d0b5fe54295bdb97589ead53d7443b3
eric.cheng <eric.cheng@psp-power.com.tw>
2022-09-13 16:58:53 +0800

    feature: install devise and set devise config

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action(:find_group, only: [:show, :edit, :update, :destroy])
+  before_action(:set_group, only: [:new])
 
   def index
     @groups = Group.order(created_at: :desc)
