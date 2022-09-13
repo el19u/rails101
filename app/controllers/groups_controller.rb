b4da581b6df136c6af55eff90fba91d1ad877326
eric.cheng <eric.cheng@psp-power.com.tw>
2022-09-13 15:18:33 +0800

    feature: implement groups#create & implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,21 @@
 class GroupsController < ApplicationController
   def index
-    @groups = Group.all
+    @groups = Group.order(created_at: :desc)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    redirect_to root_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 end
