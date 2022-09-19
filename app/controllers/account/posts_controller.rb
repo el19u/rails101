f30f48b1968683bbb89da30dbe4777c0ae1960e2
eric.cheng <eric.cheng@psp-power.com.tw>
2022-09-19 11:03:48 +0800

    feature: edit post content from my_posts

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,31 +1,7 @@
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_post, only: [:edit, :update, :destroy]
-
+  
   def index
-    @posts = current_user.posts
-  end
-
-  # def edit
-  # end
-
-  # def update
-  #   if @post.update(post_params)
-  #     redirect_to(account_posts_path, notice: "更新成功")
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to(account_posts_path, alert: "刪除成功")
-  end
-
-  private
-
-  def find_post
-    @post = Post.find(params[:id])
+    @posts = current_user.posts.recent
   end
 end
