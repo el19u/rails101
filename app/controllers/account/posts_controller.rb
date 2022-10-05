1f62dc8343981c86b52535bb76eac5377a834ec2
eric.cheng <eric.cheng@psp-power.com.tw>
2022-10-05 12:02:47 +0800

    feature: my posts page can edit & delete

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,40 +1,50 @@
 # frozen_string_literal: true
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
-  # before_action :find_group, only: [:edit, :update, :destroy]
+  before_action :find_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = current_user.posts.includes(:group).recent
   end
 
   def edit
-    @post = current_user.posts.find(params[:id])
   end
 
   def update
-    @post = current_user.posts.find(params[:id])
-
     if @post.update(post_params)
-      redirect_to account_posts_path, notice: "Post updated."
+      status = @post.status
+
+      case status
+        when "published"
+          @post.update_verify!
+        end
+
+      redirect_to(account_posts_path, notice: "更新文章成功，進入審查階段")
     else
       render :edit
     end
   end
 
   def destroy
-    @post = current_user.posts.find(params[:id])
-    @post.destroy
-    redirect_to account_posts_path, alert: "Post deleted."
-  end 
+    status = post_params[:status]
+
+    case status
+      when "delete_by_owner"
+        @post.delete_by_owner!
+      when "delete_by_user"
+        @post.delete_by_user!
+    end
+
+    redirect_to(account_posts_path, alert: "文章已被刪除")
+  end
 
   private
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :status)
   end
 
-  def find_group
-    @group = Group.find(params[:group_id])
+  def find_post
+    @post = Post.find(params[:id])
   end
-
 end
