bf10aadfc8b74bc9543176d51090c1197cc299ab
KurtHsu <kurt@psp-power.com.tw>
2022-10-12 10:52:18 +0800

    fix helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 module PostsHelper
-  def delete_by(post_user)
-    current_user_post?(post_user) ? "delete_by_post_author" : "delete_by_owner"
-  end
+  def read_authorization?(post)
+    return true if post.author?(current_user)
+    return true if post.group.owner?(current_user) && (post.block? || post.update_decline?)
+    return true if post.publish?
 
-  def read_authorization(post)
-    current_user_post?(post) || post.viewable?
+    false
   end
 
   def post_status_message(status)
@@ -14,7 +14,6 @@ module PostsHelper
     status_messages = {
       publish: "發布中文章",
       delete_by_post_author: "文章已被使用者刪除",
-      delete_by_owner: "文章已被群組管理員刪除",
       block: "文章已被管理員封鎖",
       update_verify: "文章更新中",
       update_decline: "版主拒絕更新文章",
@@ -22,13 +21,5 @@ module PostsHelper
     }
 
     status_messages[status]
-  end
-
-  def current_user_post?(post_user)
-    post_user == current_user
-  end
-
-  def group_owner?(group)
-    group.user == current_user
   end
 end
